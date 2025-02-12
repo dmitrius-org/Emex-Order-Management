228679339d71ac3589ee4cb5c4d3f4b40ab2d5fc
Дмитрий Александров <47624368+dmitrius-org@users.noreply.github.com>
2025-02-12 16:44:52 +0300

    Update PriceUpdate.pas

--- sources/Manager/App/DashBoard/PriceUpdate.pas
+++ sources/Manager/App/DashBoard/PriceUpdate.pas
@@ -1,15 +1,14 @@
-unit PriceUpdate;
+﻿unit PriceUpdate;
 
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics,
-  Controls, Forms, uniGUITypes, uniGUIAbstractClasses,
-  uniGUIClasses, uniGUIFrame, uniGUIBaseClasses, uniBasicGrid, uniDBGrid,
-  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
-  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
-  FireDAC.Stan.Async, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client;
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  uniGUITypes, uniGUIAbstractClasses, uniGUIClasses, uniGUIFrame,
+  uniGUIBaseClasses, uniBasicGrid, uniDBGrid, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
+  FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt,
+  Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TPriceUpdateT = class(TUniFrame)
@@ -20,8 +19,7 @@ type
     QueryUpdateDate: TSQLTimeStampField;
     QueryUpdateDays: TIntegerField;
     procedure UniFrameReady(Sender: TObject);
-    procedure GridDrawColumnCell(Sender: TObject; ACol, ARow: Integer;
-      Column: TUniDBGridColumn; Attribs: TUniCellAttribs);
+    procedure GridDrawColumnCell(Sender: TObject; ACol, ARow: Integer; Column: TUniDBGridColumn; Attribs: TUniCellAttribs);
   private
     { Private declarations }
   public
@@ -37,32 +35,27 @@ uses
 
 {$R *.dfm}
 
-
-
 procedure TPriceUpdateT.DataRefresh;
 begin
   Query.Close;
   Query.Open;
 end;
 
-
-
-procedure TPriceUpdateT.GridDrawColumnCell(Sender: TObject; ACol, ARow: Integer;
-  Column: TUniDBGridColumn; Attribs: TUniCellAttribs);
+procedure TPriceUpdateT.GridDrawColumnCell(Sender: TObject; ACol, ARow: Integer; Column: TUniDBGridColumn; Attribs: TUniCellAttribs);
 begin
   if (Query.FieldByName('UpdateDays').AsInteger = 0) then
   begin
-    Attribs.Font.Color:=clMaroon;
-    Attribs.Font.Style:=[fsBold, fsItalic];
-    Attribs.Color:= rgb(153,204,0);
+    Attribs.Font.Color := clMaroon;
+    Attribs.Font.Style := [fsBold, fsItalic];
+    Attribs.Color := rgb(153, 204, 0);
   end
   else
   if (Query.FieldByName('UpdateDays').AsInteger = 1) then
   begin
-    Attribs.Color:=rgb(255,219,88);
+    Attribs.Color := rgb(255, 219, 88);
   end
   else
-    Attribs.Color:=rgb(255,77,77);
+    Attribs.Color := rgb(255, 77, 77);
 
 end;
 
@@ -73,4 +66,6 @@ end;
 
 initialization
   RegisterClass(TPriceUpdateT);
+
 end.
+
